6a8106b8463a05e2dd783ae249d77df1afbc3751
Gabriel Lima <gabriellimatrajano@hotmail.com>
2016-12-14 18:40:20 -0200

    subindo os index's

--- app/controllers/robot_logs_controller.rb
+++ app/controllers/robot_logs_controller.rb
@@ -2,7 +2,7 @@ class RobotLogsController < ApplicationController
   before_action :set_robot_log, only: [:show, :destroy]
 def index
   @robot_logs = RobotLog.paginate(:page => params[:page], :per_page => 10)
-														      .order(created_at: :asc)
+                        .order(created_at: :asc)
 end
 
 def show
