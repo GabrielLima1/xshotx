aff70c3eae9dde626d05802d151b5fb5cad39c01
Gabriel Lima <gabriellimatrajano@hotmail.com>
2017-01-01 13:07:17 -0200

    subindo o insta

--- s.rb
+++ s.rb
@@ -1,5 +1,5 @@
 require 'watir-webdriver'
-require 'phantomjs'
+#require 'phantomjs'
 
 @b = Watir::Browser.new :phantomjs
 Watir.default_timeout = 90
@@ -9,9 +9,10 @@ Watir.default_timeout = 90
 
 #b = Watir::Browser.new driver
 @b.goto 'instagram.com/accounts/login/'
-sleep 3
+sleep 5
 p @b.url
 if @b.url.include? "/accounts/login/"
+	p "Entrie no If"
 	sleep 2
 	login = "pagoate"
 	senha = "123mudar@@"
@@ -27,52 +28,53 @@ if @b.url.include? "/accounts/login/"
 	sleep 2
 	@b.button(text: 'Log in').click
 end
-controle = 0
-numero = 1
-sleep 2
-begin
-	@b.goto "https://www.instagram.com/#{hashtag}/"
-	sleep 1
-rescue
-	p "Falha ao Abrir Perfil"
-end
-sleep 2
-#sleep 600
-p @b.link(text: "Profile").present?
-p @b.spans[6].text
-p @b.spans[7].text
-p @b.spans[8].text
-p @b.spans[9].text
-sleep 2
-# @b.spans[7].click
-# sleep 3
-# if @b.button(text: "Follow").present?
-# 	@b.button(text: "Follow").click
-# elsif @b.button(text: "Following").present?
-# 	@b.button(text: "Following").click
-# else
-# 	p "Erro na Primeira ação"
+p @b.url
+# controle = 0
+# numero = 1
+# sleep 2
+# begin
+# 	@b.goto "https://www.instagram.com/#{hashtag}/"
+# 	sleep 1
+# rescue
+# 	p "Falha ao Abrir Perfil"
 # end
 # sleep 2
-# ale = Random.rand(22...30)
-# controle = 0
-#
-# while ale > controle
-# 	begin
-# 		if @b.buttons(text: "Follow")[1].present?
-# 			@b.buttons(text: "Follow")[1].click
-# 			sleep 1
-# 			controle += 1
-# 			#p controle
-# 		else
-# 			sleep 2
-# 			@b.send_keys :end
-# 			sleep 2
-# 		end
-# 	rescue
-# 		#p "Voltando Daqui a Pouco!"
-# 	end
-# end
-# p controle
+# #sleep 600
+# p @b.link(text: "Profile").present?
+# p @b.spans[6].text
+# p @b.spans[7].text
+# p @b.spans[8].text
+# p @b.spans[9].text
+# sleep 2
+# # @b.spans[7].click
+# # sleep 3
+# # if @b.button(text: "Follow").present?
+# # 	@b.button(text: "Follow").click
+# # elsif @b.button(text: "Following").present?
+# # 	@b.button(text: "Following").click
+# # else
+# # 	p "Erro na Primeira ação"
+# # end
+# # sleep 2
+# # ale = Random.rand(22...30)
+# # controle = 0
+# #
+# # while ale > controle
+# # 	begin
+# # 		if @b.buttons(text: "Follow")[1].present?
+# # 			@b.buttons(text: "Follow")[1].click
+# # 			sleep 1
+# # 			controle += 1
+# # 			#p controle
+# # 		else
+# # 			sleep 2
+# # 			@b.send_keys :end
+# # 			sleep 2
+# # 		end
+# # 	rescue
+# # 		#p "Voltando Daqui a Pouco!"
+# # 	end
+# # end
+# # p controle
 p "Fim"
 @b.close
