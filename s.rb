3cadb3509a758d8ff0c2813e173ba50894edead1
Gabriel Lima <gabriellimatrajano@hotmail.com>
2017-01-02 00:24:53 -0200

    insta fase teste 3

--- s.rb
+++ s.rb
@@ -1,12 +1,18 @@
 require 'watir-webdriver'
 b = Watir::Browser.new :phantomjs
+b.driver.manage.timeouts.implicit_wait = 3 #3 seconds
 b.goto "http://instagram.com/accounts/login"
+sleep 7
 
 if b.url.include? "accounts/login"
-	sleep 7
-  b.text_field(name: "username").set "capsluuk"
-  b.text_field(name: "password").set "analima"
-  b.button(text: "Log in").click
+	sleep 4
+	body = b.body
+	body.text_fields.each do |text_field|
+	  p text_field.name
+	end
+  b.text_field(:name => 'username').when_present.set 'capsluuk'
+  b.text_field(:name => 'password').when_present.set 'analima'
+  b.button(:value => 'Submit').click
   sleep 7
 end
 p b.url
