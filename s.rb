5c202a11e2502c603822217112b2cdb321630aac
Gabriel Lima <gabriellimatrajano@hotmail.com>
2017-01-02 00:57:45 -0200

    insta fase teste 10

--- s.rb
+++ s.rb
@@ -1,12 +1,11 @@
 require 'watir-webdriver'
 
 b = Watir::Browser.new :phantomjs
-
-b.goto "instagram.com"
-
-body = b.body
-body.text_fields.each do |text_field|
-  p text_field.name
-end
-sleep 7
-p b.url
+b.window.resize_to(800, 600)
+b.goto "http://instagram.com/accounts/login"
+sleep(5)
+puts b.iframe(:class => "hiFrame").text_field(:name => "username").exists?
+b.iframe(:class => "hiFrame").text_field(:name => "username").set "capsluuk"
+b.iframe(:class => "hiFrame").text_field(:name => "password").set "analima"
+puts b.iframe(:class => "hiFrame").text_field(:name => "username").exists?
+b.iframe(:class => "hiFrame").button.click
