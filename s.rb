43f994f2b2dc5c27aad721ef95272114b0b024c1
Gabriel Lima <gabriellimatrajano@hotmail.com>
2017-01-02 00:41:08 -0200

    insta fase teste 7

--- s.rb
+++ s.rb
@@ -4,17 +4,9 @@ b = Watir::Browser.new :phantomjs
 
 b.goto "https://www.instagram.com/accounts/login/?hl=pt-br"
 
+sleep 4
+b.text_field(placeholder: "Nome de usuário").set "capsluuk"
+b.text_field(placeholder: "Senha").set "analima"
+b.button(text: "Entrar").click
 sleep 7
-body = b.body
-p body
-body.text_fields.each do |text_field|
-	p text_field.name
-end
-
-if b.url.include? "accounts/login"
-  b.text_field(name: "username").set "capsluuk"
-  b.text_field(name: "password").set "analima"
-  b.button(text: "Entrar").click
-  sleep 7
-end
 p b.url
