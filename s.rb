fc9617bae90d97b9edbbf9f66f73362dface57a2
Gabriel Lima <gabriellimatrajano@hotmail.com>
2017-01-02 01:06:00 -0200

    insta fase teste 11

--- s.rb
+++ s.rb
@@ -4,8 +4,7 @@ b = Watir::Browser.new :phantomjs
 b.window.resize_to(800, 600)
 b.goto "http://instagram.com/accounts/login"
 sleep(5)
-puts b.iframe(:class => "hiFrame").text_field(:name => "username").exists?
-b.iframe(:class => "hiFrame").text_field(:name => "username").set "capsluuk"
-b.iframe(:class => "hiFrame").text_field(:name => "password").set "analima"
-puts b.iframe(:class => "hiFrame").text_field(:name => "username").exists?
-b.iframe(:class => "hiFrame").button.click
+p b.text_field(placeholder: "Username").present?
+p b.text_field(placeholder: "Password").present?
+p b.text_field(placeholder: "Username").exists?
+p b.text_field(placeholder: "Password").exists?
