5f3bdae364d027b8bb73111ce488c8702dead9d3
Gabriel Lima <gabriellimatrajano@hotmail.com>
2017-01-13 10:22:13 -0200

    subindo o robo de notas

--- s.rb
+++ s.rb
@@ -14,22 +14,22 @@ else
     `read -s -p "#{prompt}" password; echo $password`.chomp
   end
 end
-mensagem_email = "foi APROVADO!!!
-Aguarde que logo logo seu pedido chegará em sua residência. Dúvidas sobre prazo de envio ou sobre o pedido acesse nosso campo PERGUNTAS FREQUENTES em nosso site : http://mistermattpulseiras.com.br/perguntas-frequentes/"
-mensagem_wpp = "Email já Coletado!"
+mensagem_email = "Aguarde que logo logo seu pedido chegará em sua residência. Dúvidas sobre prazo de envio ou sobre o pedido acesse nosso campo PERGUNTAS FREQUENTES em nosso site : http://mistermattpulseiras.com.br/perguntas-frequentes/"
 
 login_wpp = "admin"
 senha_wpp = "alma1505@12#12"
 
-login_email = "mistermattpulseiras"
-senha_email = "analima@123"
 
 links = []
 emails = []
 numero = 1
 antigos = 0
 novos = 0
+month = { "janeiro" => "01", "fevereiro" => "02", "março" => "03", "abril" => "04",
+        "maio" => "05", "junho" => "06", "julho" => "07", "agosto" => "08", "setembro" => "09",
+         "outubro" => "10", "novembro" => "11", "dezembro" => "12"}
 p "Aguarde..."
+
 # FAZENDO LOGIN NO WPP
 @b = Watir::Browser.new :phantomjs
 Watir.default_timeout = 90
@@ -41,106 +41,83 @@ Watir.default_timeout = 90
 @b.button(type: "submit").click
 sleep 2
 p "Login no WPP Feito"
-# FAZENDO LOGIN NO GMAIL
-@b.goto "mail.google.com"
+#http://mistermattpulseiras.com.br/wp-admin/edit.php?s&post_status=wc-processing&post_type=shop_order&action=-1&m=201701&_customer_user&filter_action=Filtrar&paged=1&action2=-1
+
+@b.goto "http://mistermattpulseiras.com.br/wp-admin/edit.php?s&post_status=wc-processing&post_type=shop_order&action=-1&m=201701&_customer_user&filter_action=Filtrar&paged=#{numero}&action2=-1"
 sleep 2
-p @b.url
-@b.text_field(name: "Email").set login_email
-@b.button(type: "submit").click
-sleep 2
-@b.text_field(name: "Passwd").set senha_email
-@b.button(text: "Sign in").click
-sleep 2
-p "Login no Gmail Feito"
-while numero < 4
-  sleep 2
-  @b.goto "http://mistermattpulseiras.com.br/wp-admin/edit.php?s&post_status=wc-processing&post_type=shop_order&action=-1&m=201701&_customer_user&filter_action=Filtrar&paged=1&action2=-1&paged=#{numero}"
-  sleep 3
-  @b.links(text: "Visualizar").each do |link|
-    links << link.href
-  end
-  p links.count
-  links.each do |link|
-    @b.goto link
+p final = @b.span(class: "total-pages").text
+final = final.to_i
+
+while numero <= final
+  begin
+    @b.goto "http://mistermattpulseiras.com.br/wp-admin/edit.php?s&post_status=wc-processing&post_type=shop_order&action=-1&m=201701&_customer_user&filter_action=Filtrar&paged=#{numero}&action2=-1"
     sleep 2
-    if @b.p(text: "Email Enviado!").present?
-      if @b.span(text: "Processando").present?
-        @b.span(text: "Processando").click
-        sleep 2
-        @b.div(text: "Aguardando").click
-        sleep 1
-        @b.button(text: "Salvar Pedido").click
-        sleep 5
-      else
-        p "Já fazido!"
-        break
-        break
-      end
-    elsif @b.p(text: "Email já Coletado!").present?
-      sleep 2
+    @b.links(text: "Visualizar").each do |link|
+      links << link.href
+    end
+    p links.length
+    numero += 1
+  rescue
+  end
+end
+p links.length
+
+links.each do |link|
+  begin
+    @b.goto link
+    sleep 3
+    if @b.p(text: "Email Enviado!").present? && @b.span(text: "Processando").present?
       @b.span(text: "Processando").click
       sleep 2
       @b.div(text: "Aguardando").click
       sleep 1
-      @b.textarea(name: "order_note"). set "Email Enviado!"
-      sleep 1
-      #@b.select_list(name: 'order_note_type').select 'Nota para o cliente'
-      #sleep 1
-      @b.link(text: "Adicionar").click
-      sleep 2
-      @b.send_keys :home
-      sleep 2
       @b.button(text: "Salvar Pedido").click
       sleep 5
-      # VER SE ESTA TUDO OK SE NAÕ NÃO PRECISA COLOCAR O SLEEP
     else
       # VER SE ESTA TUDO OK SE NAÕ NÃO PRECISA COLOCAR O SLEEP
       sleep 2
-      div = @b.div(id: "order_data")
-      numero_pedido = div.h2s.first.text.gsub("Detalhes do Pedido ", "")
+      p "Entrei"
       @b.links(text: "Editar").first.click
-      sleep 2
-      nome = @b.text_field(id: "_billing_first_name").value
-      email = @b.text_field(id: "_billing_email").value
-      sleep 2
-      @b.goto "gmail.com"
-      sleep 3
-      @b.div(text: "ESCREVER").click
       sleep 1
-      form = @b.forms[3]
-      form.text_fields[1].set email
-      form.text_field(name: "subjectbox").set "Confimação do Pedido"
-      @b.send_keys :tab
-      sleep 2
-      @b.send_keys "Olá #{nome}"
-      @b.send_keys [:shift, :enter]
-      @b.send_keys "O Pedido #{numero_pedido} #{mensagem_email}"
-      sleep 2
-      @b.send_keys [:control, :enter]
-      sleep 2
-      #VOLTANDO PARA O WPP
-
-      @b.goto link
+      nome = @b.text_field(id: "_billing_first_name").value
+      @b.refresh
       sleep 2
       @b.span(text: "Processando").click
       sleep 2
       @b.div(text: "Aguardando").click
+      sleep 2
+      div = @b.div(id: "order_data")
+      numero_pedido = div.h2s.first.text.gsub("Detalhes do Pedido ", "")
+      data = @b.p(class: "order_number").text
+      data = data.split("em").last.split("às").first.lstrip.strip
+      data = data.gsub(" de ","/")
+      mes = data.split("/")[1]
+      data = data.gsub("#{mes}", month["#{mes}"])
+      mensagem = "O Pedido #{numero_pedido} foi APROVADO na Data #{data}".upcase
+      @b.textarea(name: "order_note"). set mensagem
+      @b.send_keys [:shift, :enter]
+      @b.send_keys [:shift, :enter]
+      @b.send_keys "#{nome},"
+      @b.send_keys [:shift, :enter]
+      @b.send_keys "#{mensagem_email}"
       sleep 1
+      @b.select_list(name: 'order_note_type').select 'Nota para o cliente'
+      sleep 1
+      @b.link(text: "Adicionar").click
+      sleep 3
       @b.textarea(name: "order_note"). set "Email Enviado!"
+      sleep 2
+      @b.select_list(name: 'order_note_type').select 'Nota privada'
       sleep 1
-      #@b.select_list(name: 'order_note_type').select 'Nota para o cliente'
-      #sleep 1
       @b.link(text: "Adicionar").click
       sleep 2
       @b.send_keys :home
       sleep 2
       @b.button(text: "Salvar Pedido").click
       sleep 6
-      p "Feito"
     end
+  rescue
   end
-  numero += 1
-  links.clear
 end
-p "Acabouuuuuu!"
 @b.close
+p "Terminei"
